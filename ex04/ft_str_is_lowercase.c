5d268cd64606f3ca22f383df4a348ed6e9d8679c
JalitiQ <contact@jullienl.com>
2025-07-01 18:51:55 +0200

    ex04 updated

--- ex04/ft_str_is_lowercase.c
+++ ex04/ft_str_is_lowercase.c
@@ -6,9 +6,11 @@
 /*   By: lilian <lilian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 23:47:15 by lilian            #+#    #+#             */
-/*   Updated: 2025/06/30 23:48:44 by lilian           ###   ########.fr       */
+/*   Updated: 2025/07/01 18:51:38 by lilian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_str_is_lowercase(char *str)
 {
@@ -16,9 +18,13 @@ int	ft_str_is_lowercase(char *str)
 	int	ret;
 
 	i = 0;
+	if (str[i] == '\0')
+	{
+		ret = 1;
+	}
 	while (str[i])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] == '\0'))
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			ret = 1;
 		}
